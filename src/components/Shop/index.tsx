d225a737e303c8462ad47492600f6e499388932b
bilalashrafali123 <ashrafalibilal385@gmail.com>
2025-05-28 14:13:50 +0500

    updated...

--- src/components/Shop/index.tsx
+++ src/components/Shop/index.tsx
@@ -3,69 +3,72 @@ import Header from "../Header";
 import Navbar from "../Navbar";
 import UpperHeader from "../UpperHeader";
 import FeaturedProducts from "../FeaturedProducts";
-import "./index.scss";
 import Footer from "../Footer";
+import "./index.scss";
 
 const Shop = () => {
   return (
     <>
-    <UpperHeader />
-    <Header />
-    <Navbar />
-    <Banner title="shop" />
-  <div className="shop-wrapper">
-      <div className="container">
-        <div className="shop-inner">
-          <div className="shop-sidebar">
-              <h4>FILTER BY PRICE</h4>
-        <div className="filter-block">
-              <ul>
-                <li><input type="checkbox" /> All Price</li> 
-                <li><input type="checkbox" /> $0 - $100</li>
-                <li><input type="checkbox" /> $100 - $200</li>
-                <li><input type="checkbox" /> $200 - $300</li>
-                <li><input type="checkbox" /> $300 - $400</li>
-                <li><input type="checkbox" /> $400 - $500</li>
-              </ul>
+      <UpperHeader />
+      <Header />
+      <Navbar />
+      <Banner title="shop" />
+      <div className="shop-wrapper">
+        <div className="container">
+          <div className="shop-inner">
+            <div className="shop-sidebar">
+              <div className="filter-block">
+                <h4>FILTER BY PRICE</h4>
+                <ul>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>All Price</span><span className="count">(12)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>$0 - $100</span><span className="count">(5)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>$100 - $200</span><span className="count">(3)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>$200 - $300</span><span className="count">(2)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>$300 - $400</span><span className="count">(1)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>$400 - $500</span><span className="count">(1)</span></div></li>
+                </ul>
+              </div>
+
+              <div className="filter-block">
+                <h4>FILTER BY COLOR</h4>
+                <ul>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>All Color</span><span className="count">(10)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>Black</span><span className="count">(4)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>White</span><span className="count">(2)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>Red</span><span className="count">(1)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>Blue</span><span className="count">(2)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>Green</span><span className="count">(1)</span></div></li>
+                </ul>
+              </div>
+
+              <div className="filter-block">
+                <h4>FILTER BY SIZE</h4>
+                <ul>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>All Size</span><span className="count">(8)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>XS</span><span className="count">(1)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>S</span><span className="count">(2)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>M</span><span className="count">(3)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>L</span><span className="count">(1)</span></div></li>
+                  <li><input type="checkbox" /><div className="label-wrap"><span>XL</span><span className="count">(1)</span></div></li>
+                </ul>
+              </div>
             </div>
-              <h4>FILTER BY COLOR</h4>
-            <div className="filter-block">
-              <ul>
-                <li><input type="checkbox" /> All Color</li>
-                <li><input type="checkbox" /> Black</li>
-                <li><input type="checkbox" /> White</li>
-                <li><input type="checkbox" /> Red</li>
-                <li><input type="checkbox" /> Blue</li>
-                <li><input type="checkbox" /> Green</li>
-              </ul>
-            </div>
-              <h4>FILTER BY SIZE</h4>
-            <div className="filter-block">
-              <ul>
-                <li><input type="checkbox" /> All Size</li>
-                <li><input type="checkbox" /> XS</li>
-                <li><input type="checkbox" /> S</li>
-                <li><input type="checkbox" /> M</li>
-                <li><input type="checkbox" /> L</li>
-                <li><input type="checkbox" /> XL</li>
-              </ul>
-            </div>
-          </div>
-          <div className="shop-content">
-            <div className="shop-toolbar">
-              <button>Sorting</button>
-              <button>Showing</button>
-            </div>
-             <div className="shop-grid">
-              <FeaturedProducts />
-              <FeaturedProducts />
-              <FeaturedProducts />
+
+            <div className="shop-content">
+              <div className="shop-toolbar">
+                <button>Sorting</button>
+                <button>Showing</button>
+              </div>
+              <div className="shop-grid">
+                <FeaturedProducts />
+                <FeaturedProducts />
+                <FeaturedProducts />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 };
